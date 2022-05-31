f4f235485f124617f8fc4b640e953a89e1e95450
Beksultan Chokonov <beksultan.chokonov.alt@gmail.com>
2022-05-31 15:16:16 +0600

    Add homework

---
+++ 1/script.js
@@ -0,0 +1,32 @@
+// 1. Напишите функцию, которая может преобразовывать строку в число - 10 баллов
+
+// const strToNumber = (number) => {
+//   str = Number(number);
+//   return str;
+// };
+// console.log(strToNumber("1385"));
+
+// 2. Напишите функцию, которая проверяет число четное или не четное: если четное возвращает **`true`** иначе **`false`** - 10 баллов
+
+// function isEven(number) {
+//   var x;
+//   if (number % 2 == 0) {
+//     x = true;
+//   } else {
+//     x = false;
+//   }
+//   return x;
+// }
+
+// alert(isEven(12));
+// console.log(isEven(12));
+
+// 3. Сделайте функцию, которая определяет, есть в массиве элемент с заданным текстом или нет. Функция первым параметром должна принимать текст элемента, а вторым - массив, в котором делается поиск. Функция должна возвращать **`true`** или **`false` -** 10 баллов
+
+// const hasElement = (aiba, array) => {
+//   let result = array.includes(aiba);
+//   return result;
+// };
+// let arr = ["Beku", "Talant", "Aibek"];
+// console.log(hasElement("Beku", arr));
+// console.log(hasElement("Aibek", arr));
